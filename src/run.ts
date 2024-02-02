8b1e291ef9d315fe65887a68a7cdb0ac9386151b
Finn O'Donoghue <finnodonoghue@gmail.com>
2024-02-03 04:58:41 +1100

    commit

--- src/run.ts
+++ src/run.ts
@@ -1,18 +1,18 @@
 import Hero from "@ulixee/hero";
-import ExecuteJsPlugin from "./exec-with-callback/index";
+import ExecuteJsPlugin from "./exec-with-callback";
 
 async function main() {
     const hero = new Hero({
         showChrome: true,
     });
-    // hero.use(ExecuteJsPlugin);
+    hero.use(ExecuteJsPlugin);
 
     await hero.goto("https://ulixee.org");
     await hero.activeTab.waitForPaintingStable();
-    // const divs = await hero.executeJs(() => {
-    //     return document.querySelectorAll("div").length;
-    // });
-    // console.log(divs);
+    const divs = await hero.executeJs(() => {
+        return document.querySelectorAll("div").length;
+    });
+    console.log(divs);
     await hero.close();
 }
 
